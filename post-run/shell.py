017c4e5d29b53d91a876bdb726cebd023e1bda85
Jason Kendall <coolacid@gmail.com>
2020-05-19 11:37:02 -0400

    use shell if we're passing a string

--- post-run/shell.py
+++ post-run/shell.py
@@ -12,13 +12,15 @@ class postrun_shell(baseclass):
         self.feedname = feedname
 
     def hook(self):
+        shell = False
         if isinstance(self.hook_config['command'], str):
             command = self.hook_config['command'].split()
+            shell = True
         elif isinstace(self.hook_config['command'], list):
             command = self.hook_config['command']
         else:
             logging.error("Command must be a string, or list, see subprocess.run")
             return
-        output = subprocess.run(self.hook_config['command'], stdout = subprocess.PIPE, text = True)
+        output = subprocess.run(self.hook_config['command'], stdout = subprocess.PIPE, text = True, shell=shell)
         for line in output.stdout.split("\n"):
             logging.debug(line)
